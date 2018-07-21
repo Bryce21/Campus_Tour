f1ec60f058f16592321892efa8a5b5e3b9fa8b5b
Bryce <reinhard.bryce.21@gmail.com>
2018-07-21 16:43:13 -0400

    R/W from db done

--- app.js
+++ app.js
@@ -14,10 +14,13 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+app.set('view engine', 'ejs');
+
 //Define the Schemas
 var studentSchema = new mongoose.Schema({
 	name: String,
-	grade: String,
+	major: String,
 	age: Number
 });
 
@@ -35,57 +38,79 @@ var user = mongoose.model("Login", userSchema);
  	res.sendFile(__dirname + "/main.html");
  });
 
+ app.get('/computer_science.html', function(req, res) {
+ 	res.sendFile(__dirname + "/computer_science.html");
+ });
+
+ app.get('/architecture.html', function(req, res) {
+ 	res.sendFile(__dirname + "/architecture.html");
+ });
+
+ app.get('/engineering.html', function(req, res) {
+ 	res.sendFile(__dirname + "/engineering.html");
+ });
+
  app.get('/main.html', function(req, res) {
  	res.sendFile(__dirname + "/main.html");
  });
 
-app.get('/math.html', function(req, res) {
-	res.sendFile(__dirname + "/math.html");
-});
+ app.get('/math.html', function(req, res) {
+ 	res.sendFile(__dirname + "/math.html");
+ });
 
-app.get('/student_life.html', function(req, res) {
-	res.sendFile(__dirname + "/student_life.html");
-});
+ app.get('/student_life.html', function(req, res) {
+ 	res.sendFile(__dirname + "/student_life.html");
+ });
 
-app.get('/login_page.html', function(req, res) {
-	res.sendFile(__dirname + "/login_page.html");
-});
+ app.get('/login_page.html', function(req, res) {
+ 	res.sendFile(__dirname + "/login_page.html");
+ });
 
-app.get('/admissions.html', function(req, res) {
-	res.sendFile(__dirname + "/admissions.html");
-});
+ app.get('/admissions.html', function(req, res) {
+ 	res.sendFile(__dirname + "/admissions.html");
+ });
 
-app.get('/about_us.html', function(req, res) {
-	res.sendFile(__dirname + "/about_us.html");
-});
+ app.get('/about_us.html', function(req, res) {
+ 	res.sendFile(__dirname + "/about_us.html");
+ });
 
-app.post("/addStudent", (req, res) => {
-	var myData = new student(req.body);
-	myData.save()
-	.then(item => {
-		res.send("item saved to database");
+ app.get('/notable_graduates.ejs', (req, res) => {
+ 	db.collection('students').find().toArray((err, result) => {
+ 		if (err) return console.log(err)
+		// renders index.ejs
+		res.render(__dirname+'/notable_graduates.ejs', {students: result})
 	})
-	.catch(err => {
-		res.status(400).send("unable to save to database");
-	});
-});
+ })
 
+ app.post("/addStudent", (req, res) => {
+ 	console.log(req.body);
+ 	var myData = new student(req.body);
+ 	myData.save()
+ 	.then(item => {
+ 		res.sendFile(__dirname+'/admissions.html');
+ 	})
+ 	.catch(err => {
+ 		res.status(400).send("unable to save to database");
+ 	});
+ });
 
-app.post("/login", (req, res) => {
-	console.log(req);
-	var myData = new user(req.body);
-	myData.save()
-	.then(item => {
-		res.send("User could log in if db setup correctly.");
-	})
-	.catch(err => {
-		res.status(400).send("unable to save to database");
-	});
-});
+ 
 
-app.listen(port, () => {
-	console.log("Server listening on port " + port);
-});
+ app.post("/login", (req, res) => {
+ 	console.log(req);
+ 	var myData = new user(req.body);
+ 	myData.save()
+ 	.then(item => {
+ 		res.send("User could log in if db setup correctly.");
+ 	})
+ 	.catch(err => {
+ 		res.status(400).send("unable to save to database");
+ 	});
+ });
+
+ app.listen(port, () => {
+ 	console.log("Server listening on port " + port);
+ });
 
 
 
